9b0f8b93cfe24656651a8c459f9696ad7ddf6695
Adrian Zimmermann <zadrian@web.de>
2020-08-25 14:51:24 +0200

    run swift test --generate-linuxmain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -5,6 +5,8 @@ import SwimTests
 import VisualTests
 
 var tests = [XCTestCaseEntry]()
+tests += PerformanceTests.__allTests()
 tests += SwimTests.__allTests()
+tests += VisualTests.__allTests()
 
 XCTMain(tests)
